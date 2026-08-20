5197497c69643079b6e4ca5906d831b224b8d9b2
PRACHAYA04 <saptasavee_p@silpakorn.edu>
2026-08-20 05:20:24 +0000

    handon-week5

--- goodthing/src/App.js
+++ goodthing/src/App.js
@@ -1,15 +1,46 @@
-import Header from "./components/Header";
-import MovieList from "./components/Movielist";
-import Footer from "./components/Footer";   
+import React from "react";
+import ProfileCard from "./components/ProfileCard";
+import "./App.css";
 
-import Greeting from './Greeting';
-
+const members = [
+  {
+    id: 1,
+    name: "กรวิช",
+    nickname: "โอม",
+    major: "เทคโนโลยีสารสนเทศ",
+    favorites: ["กาแฟ", "เกม", "เขียนโปรแกรม"],
+  },
+  {
+    id: 2,
+    name: "ศิวกร",
+    nickname: "เจ๋ง",
+    major: "เทคโนโลยีสารสนเทศ",
+    favorites: ["ฟุตบอล", "หมูกระทะ", "เพลง"],
+  },
+  {
+    id: 3,
+    name: "ปราชญา",
+    nickname: "โอดิน",
+    favorites: ["แมว", "ท่องเที่ยว", "ถ่ายรูป"],
+  },
+];
 function App() {
+
   return (
-    <div>
-      <Greeting name="ฝน" year={3} />
-      <Greeting name="เต้ย" year={2} />
-      <Greeting name="มายด์" year={4} />
+    <div className="container">
+      <h1>การ์ดสมาชิกกลุ่ม</h1>
+
+      <div className="ProfileCard">
+        {members.map((member) => (
+          <ProfileCard
+            key={member.id}
+            name={member.name}
+            nickname={member.nickname}
+            major={member.major}
+            favorites={member.favorites}
+          />
+        ))}
+      </div>
     </div>
   );
 }
